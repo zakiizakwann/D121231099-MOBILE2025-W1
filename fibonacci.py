71eeb9359f72906ce811e082119b5990fcd3381a
zakiizakwann <dzakwandzaky20@gmail.com>
2025-08-25 09:38:14 +0800

    HANDS-ON: FIBONACCI

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Masukkan jumlah bilangan Fibonacci: "))
+fibonacci(n)
